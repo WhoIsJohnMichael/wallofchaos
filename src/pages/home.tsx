96edcc051cae262a8a5b1420f35097611fe43bf1
santos.johnmichael.t.3489 <santos.johnmichael.t.3489@gmail.com>
2024-09-01 09:49:23 +0000

    minor add

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,5 @@
-import { Link } from "react-router-dom"
-import { Bars3Icon } from "@heroicons/react/16/solid"
-import { XMarkIcon } from "@heroicons/react/16/solid"
-import { useState } from "react"
-import { clsx } from "clsx"
 import { ArrowUpRightIcon } from "@heroicons/react/16/solid"
+import { Link} from "react-router-dom";
 import { HomeNavbar } from "../components/homenavbar"
 
 export const Home = () => {
@@ -14,10 +10,10 @@ export const Home = () => {
         <div className="flex flex-col justify-center items-center text-center ">
           <h1 className="text-4xl lg:text-8xl text-blue-300 font-bold mt-20">Wall Of Chaos</h1>
           <h2 className="mt-2 text-sm lg:text-3xl font-medium">Chaos? We've got it covered.</h2>
-          <button className="text-lg bg-slate-600 px-6 py-3 rounded-full mt-6">
+          <Link to={"/register"} className="text-lg bg-slate-600 px-6 py-3 rounded-full mt-6">
               Register Now
               <ArrowUpRightIcon className="inline-block ml-1 mb-1 size-6"/>
-          </button>
+          </Link>
         </div>
       </div>
     </>
