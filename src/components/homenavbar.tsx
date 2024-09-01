d121468104e051e4d70eb6e1d418d6e0830d8769
santos.johnmichael.t.3489 <santos.johnmichael.t.3489@gmail.com>
2024-09-01 09:43:08 +0000

    separated file for navbars and added hero

--- src/components/homenavbar.tsx
+++ src/components/homenavbar.tsx
@@ -1,7 +1,46 @@
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import { clsx } from "clsx";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const HomeNavbar = () => {
+  const [showHomeNav, toggleShowHomeNav] = useState(true);
+  const showNav = (prev:boolean) => {
+    toggleShowHomeNav(!prev);
+  }
+
   return (
-    <>
-    
+    <>  
+      <div className={clsx("fixed", showHomeNav && "-translate-y-full" ,"duration-300 transition-all flex flex-col h-dvh w-dvw bg-slate-700 z-[5] p-8 gap-5")}>
+        <div className="flex justify-end">
+          <button className="p-4" onClick={() => showNav(showHomeNav)}>
+            <XMarkIcon className="w-6 h-6 fill-white" />
+          </button>
+        </div>
+        <ul className="flex flex-col lg:hidden gap-6 p-2 items-center">
+          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/beta"}>Home</Link>
+          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/about"}>About</Link>
+          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/contacts"}>Contact</Link>
+          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/login"}>Login</Link>
+        </ul>
+      </div>
+      <div className="p-8 lg:py-10 lg:px-16 font-display">
+        <nav className="relative flex justify-end items-center">
+          <img src="woc-dark.png" className="w-14 lg:w-28 absolute left-0 " alt="logo" />
+          <button className="p-4" onClick={() => showNav(showHomeNav)}>
+            <Bars3Icon className="w-6 h-6 lg:hidden" />
+          </button>
+          <ul className="hidden lg:flex lg:justify-end gap-4 p-2 items-center">
+            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4 w-24" to={"/beta"}>Home</Link>
+            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4 w-24" to={"/about"}>About</Link>
+            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4 w-24" to={"/contacts"}>Contact</Link>
+            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4" to={"/login"}>
+              <div className="bg-slate-700 hover:bg-slate-600 px-4 py-2 rounded-full">Login</div>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+      
     </>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,44 +3,22 @@ import { Bars3Icon } from "@heroicons/react/16/solid"
 import { XMarkIcon } from "@heroicons/react/16/solid"
 import { useState } from "react"
 import { clsx } from "clsx"
+import { ArrowUpRightIcon } from "@heroicons/react/16/solid"
+import { HomeNavbar } from "../components/homenavbar"
 
 export const Home = () => {
-
-  const [showHomeNav, toggleShowHomeNav] = useState(true);
-  const showNav = (prev:boolean) => {
-    toggleShowHomeNav(!prev);
-  }
-
   return (
-    <>  
-      <div className={clsx("fixed", showHomeNav && "-translate-y-full" ,"duration-300 transition-all flex flex-col h-dvh w-dvw bg-slate-700 z-[5] p-8 gap-5")}>
-        <div className="flex justify-end">
-          <button className="p-4" onClick={() => showNav(showHomeNav)}>
-            <XMarkIcon className="w-6 h-6 fill-white" />
+    <>
+      <HomeNavbar/>
+      <div className="p-8 lg:py-10 lg:px-16 text-slate-50">
+        <div className="flex flex-col justify-center items-center text-center ">
+          <h1 className="text-4xl lg:text-8xl text-blue-300 font-bold mt-20">Wall Of Chaos</h1>
+          <h2 className="mt-2 text-sm lg:text-3xl font-medium">Chaos? We've got it covered.</h2>
+          <button className="text-lg bg-slate-600 px-6 py-3 rounded-full mt-6">
+              Register Now
+              <ArrowUpRightIcon className="inline-block ml-1 mb-1 size-6"/>
           </button>
         </div>
-        <ul className="flex flex-col lg:hidden gap-6 p-2 items-center">
-          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/beta"}>Home</Link>
-          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/about"}>About</Link>
-          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/contacts"}>Contact</Link>
-          <Link className="hover:font-semibold transition-all duration-100 ease-in-out" to={"/login"}>Login</Link>
-        </ul>
-      </div>
-      <div className="p-8 lg:py-10 lg:px-16 font-display h-full">
-        <nav className="relative flex justify-end items-center">
-          <img src="woc-dark.png" className="w-14 lg:w-28 absolute left-0 " alt="logo" />
-          <button className="p-4" onClick={() => showNav(showHomeNav)}>
-            <Bars3Icon className="w-6 h-6 lg:hidden" />
-          </button>
-          <ul className="hidden lg:flex lg:justify-end gap-4 p-2 items-center">
-            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4 w-24" to={"/beta"}>Home</Link>
-            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4 w-24" to={"/about"}>About</Link>
-            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4 w-24" to={"/contacts"}>Contact</Link>
-            <Link className="hover:font-semibold transition-all duration-100 ease-in-out text-center px-4" to={"/login"}>
-              <div className="bg-slate-600 hover:bg-slate-700 px-4 py-2 rounded-full">Login</div>
-            </Link>
-          </ul>
-        </nav>
       </div>
     </>
   )
